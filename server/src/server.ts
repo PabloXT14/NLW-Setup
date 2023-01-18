7d795f1dfeb260451f1e37ef196ae14f7292e604
Pablo Alan <pabloxt14@gmail.com>
2023-01-18 13:47:10 -0300

    feat: setup server finished

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,20 @@
 import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import { PrismaClient } from '@prisma/client';
 
-const app = Fastify();
 const PORT = 3333;
+const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', (request, response) => {
-  return response.status(200).send("Hello, world!");
+app.register(cors);
+
+app.get('/', async (request, response) => {
+  const habits = await prisma.habit.findMany();
+
+  return response.status(200).send(habits);
 });
 
-app.listen({
-  port: PORT,
-}, () => console.log(`Server is running on port ${PORT}`));
+app.listen(
+  { port: PORT },
+  () => console.log(`Server is running on port ${PORT}`)
+);
