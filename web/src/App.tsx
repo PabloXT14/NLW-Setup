92ab73785ac63376cf591c7de0b402dd94fd8fa3
Pablo Alan <pabloxt14@gmail.com>
2023-01-21 18:38:38 -0300

    feat: implementing homepage structure

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,25 @@
+import './styles/global.css';
+import { Plus } from 'phosphor-react';
+import logoImg from './assets/logo.svg';
+
 export function App() {
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        {/* HEADER */}
+        <div className="w-full max-w-3xl mx-auto flex items-center justify-between">
+          <img src={logoImg} alt="Logo NLW Habits" />
+
+          <button
+            type="button"
+            className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3"
+          >
+            <Plus size={20} className="text-violet-500" />
+            Novo hábito
+          </button>
+        </div>
+        {/* CONTENT */}
+      </div>
     </div>
   )
 }
